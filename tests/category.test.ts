86ce0b3b3e4210f09138ca5072e154fb4a97930a
Glauco Villas Boas <glappsmobile@gmail.com>
2021-12-20 04:15:37 -0300

    added tests for routes categoy and discipline

---
+++ tests/category.test.ts
@@ -0,0 +1,21 @@
+import { getConnection } from "typeorm";
+import supertest from "supertest";
+import "../src/setup.ts";
+import app, { init } from "../src/app";
+
+
+beforeAll(async () => {
+    await init();
+});
+
+afterAll(async () => {
+    await getConnection().close();
+});
+
+describe("GET /categories", () => {
+    test("returns 200", async () => {
+        const result = await supertest(app).get("/categories");
+      expect(result.status).toEqual(200);
+      expect(Array.isArray(result.body)).toBe(true);
+    });
+});
---
+++ tests/teacher.test.ts
@@ -0,0 +1,20 @@
+import { getConnection } from 'typeorm';
+import supertest from 'supertest';
+import '../src/setup.ts';
+import app, { init } from '../src/app';
+
+beforeAll(async () => {
+  await init();
+});
+
+afterAll(async () => {
+  await getConnection().close();
+});
+
+describe('GET /teachers', () => {
+  test('returns 200', async () => {
+    const result = await supertest(app).get('/disciplines');
+    expect(result.status).toEqual(200);
+    expect(Array.isArray(result.body)).toBe(true);
+  });
+});
